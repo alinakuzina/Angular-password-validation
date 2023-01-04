b69588083de4256c0ca2b0bad53d995f3ae57193
alinakuzina <kuzina.lg.4.1@gmail.com>
2023-01-04 12:51:35 +0100

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContainerComponent } from './components/container/container.component';
+import { ContainerComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [AppComponent, ContainerComponent],
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { OnInit } from '@angular/core';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class ContainerComponent implements OnInit {
+  public show: boolean = false;
+  public isTyped: boolean = false;
+  public strength: string = '';
+  public isValidPassword: boolean = false;
+  public isCheckClicked: boolean = false;
+
+  form = new FormGroup({
+    password: new FormControl<string>(''),
+  });
+
+  switchShowPassword() {
+    this.show = !this.show;
+  }
+
+  validatePassword() {
+    this.isTyped = this.form.value.password ? true : false;
+    this.isCheckClicked = false;
+    if (!this.form.value.password) {
+      this.strength = '';
+      this.isValidPassword = false;
+    } else if (this.form.value.password.length < 8) {
+      this.strength = 'short';
+      this.isValidPassword = false;
+    } else if (
+      /^[0-9]+$/.test(this.form.value.password) ||
+      /^[a-z]+$/i.test(this.form.value.password) ||
+      /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/i.test(this.form.value.password)
+    ) {
+      this.strength = 'easy';
+      this.isValidPassword = false;
+    } else if (
+      /^[A-Z0-9]*$/i.test(this.form.value.password) ||
+      /^[A-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/i.test(
+        this.form.value.password
+      ) ||
+      /^[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/.test(
+        this.form.value.password
+      )
+    ) {
+      this.isValidPassword = false;
+      this.strength = 'medium';
+    } else if (
+      /^[A-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/i.test(
+        this.form.value.password
+      )
+    ) {
+      this.isValidPassword = true;
+      console.log(this.isValidPassword);
+      this.strength = 'strong';
+    }
+  }
+
+  submit() {
+    console.log(this.form.value);
+    this.isCheckClicked = true;
+  }
+
+  newCheck() {
+    this.isTyped = this.form.value.password ? true : false;
+    this.isCheckClicked = false;
+    this.strength = '';
+    this.isValidPassword = false;
+    this.form.reset();
+  }
+  ngOnInit(): void {}
+}
